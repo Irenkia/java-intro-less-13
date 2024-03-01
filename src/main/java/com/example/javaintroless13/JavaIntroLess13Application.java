791200a1f7b5975d9bda7f0347f326fd9cfa383f
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-03-01 18:52:34 +0100

    create project Spring Boot

---
+++ src/main/java/com/example/javaintroless13/JavaIntroLess13Application.java
@@ -0,0 +1,13 @@
+package com.example.javaintroless13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIntroLess13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIntroLess13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaintroless13/JavaIntroLess13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaintroless13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIntroLess13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
